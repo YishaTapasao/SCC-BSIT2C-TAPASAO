d37d4d18f04d9f734cf831352c247948d899aa43
YishaTapasao <tapasaoy@gmail.com>
2024-08-31 21:55:18 +0800

    intial commit

---
+++ src/tapasao/Tapasao.java
@@ -0,0 +1,66 @@
+package tapasao;
+
+import java.util.Scanner;
+
+public class Tapasao {
+    public static void main(String[] args) {
+
+                
+//GRADES   
+//  Grades gr = new Grades();
+//  gr.addGrades(1011, "Yisha", 1.2, 3.2, 1.2, 2.1);
+//  gr.viewGrades();
+//  Grades gr1 = new Grades();
+//  gr1.addGrades(1012, "Yashmil", 1.2, 3.2, 1.2, 2.1);
+//  gr1.viewGrades();  
+//    
+//   }
+     
+
+//GRADE
+    Grade grade = new Grade();
+    grade.genGrade();
+    
+
+
+//PRODUCT
+//      Products pr = new Products();
+//       pr.addProduct(10011, "Soap", 20, 30, 20.00);
+//       pr.viewProduct();
+//       
+//       Products pr1 = new Products();
+//       pr1.addProduct(10012, "Cards", 0, 60, 50.00);
+//       pr1.viewProduct();
+//
+//
+//    }
+
+
+
+
+//AGE
+//Scanner input = new Scanner (System.in);
+//
+//String name;
+//int byear;
+//
+//        System.out.print("Enter your name: ");
+//        name = input.nextLine();
+//        System.out.print("Enter your birth year: ");
+//        byear = input.nextInt();
+//        System.out.print("Hello "+name+"\nYour age is "+(2024-byear));
+//
+
+
+
+
+//NETPAY
+// NetPay netPay = new NetPay();
+// netPay.Info();
+    
+
+
+
+    }
+    
+}
